ceeeb1c8ed2d97427229de701e86c352e6ac8f6f
machan201708 <1084109336@qq.com>
2019-03-21 23:46:42 +0800

    update

--- src/range-slider/ngx-rangeSlider.module.ts
+++ src/range-slider/ngx-rangeSlider.module.ts
@@ -1,11 +1,15 @@
+import {CommonModule} from "@angular/common";
 import {NgModule} from '@angular/core';
 import {RangeSliderComponent} from './range-slider.component';
 
 @NgModule({
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   declarations: [RangeSliderComponent],
   entryComponents: [RangeSliderComponent],
-  exports: [RangeSliderComponent]
+  exports: [RangeSliderComponent],
+  providers: []
 })
 export class NgxRangeSliderModule {
 
